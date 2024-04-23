c3e0f2439da389c11628a04f84fc44c0ce813233
ryanrare <ryan.hollmes@gmail.com>
2024-04-23 13:05:25 -0300

    (feat) creating login function

--- users/tests.py
+++ users/tests.py
@@ -2,6 +2,7 @@ import json
 from django.test import TestCase
 from rest_framework.test import APIClient, APITestCase
 from rest_framework import status
+from .models import User
 
 
 class RegisterViewTestCase(TestCase):
@@ -21,6 +22,17 @@ class RegisterViewTestCase(TestCase):
 
 
 class LoginViewTestCase(APITestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+        user_data = {
+            "name": "testuser",
+            "email": "g@g",
+            "password": "g"
+        }
+        response = self.client.post('/users/register/', user_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
     def test_login_failure(self):
         invalid_login_data = {
             "email": "rururururu",
@@ -29,4 +41,15 @@ class LoginViewTestCase(APITestCase):
 
         response = self.client.post('/users/login/', invalid_login_data, format='json')
 
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_login_success(self):
+        # Dados de login válidos
+        login_data = {
+            "email": "g@g",
+            "password": "g"
+        }
+
+        response = self.client.post('/users/login/', login_data, format='json')
+
+        self.assertIn('jwt', response.data)
--- users/views.py
+++ users/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render
-
 from rest_framework.response import Response
 from rest_framework.status import HTTP_201_CREATED
+from rest_framework.exceptions import AuthenticationFailed
 from rest_framework.views import APIView
 
 from .serializers import UserSerializer
+from .models import User
+
+import datetime
+import jwt
 
 
 class RegisterView(APIView):
@@ -13,3 +16,24 @@ class RegisterView(APIView):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=HTTP_201_CREATED)
+
+
+class LoginView(APIView):
+    def post(self, request):
+        email = request.data.get('email')
+        password = request.data.get('password')
+
+        user = User.objects.filter(email=email).first()
+
+        if user is None or not user.check_password(password):
+            raise AuthenticationFailed('Unable to log in with the data provided')
+
+        payload = {
+            'id': user.id,
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=360),
+            'iat': datetime.datetime.utcnow()
+        }
+
+        token = jwt.encode(payload, 'secret', algorithm='HS256')
+
+        return Response({"jwt": token})
