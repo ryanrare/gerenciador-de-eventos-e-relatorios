44d46d2cac7d638dc79204f2dbe110347816960d
ryanrare <ryan.hollmes@gmail.com>
2024-04-23 12:48:03 -0300

    (test) creating test login

--- users/tests.py
+++ users/tests.py
@@ -1,6 +1,6 @@
 import json
 from django.test import TestCase
-from rest_framework.test import APIClient
+from rest_framework.test import APIClient, APITestCase
 from rest_framework import status
 
 
@@ -9,7 +9,6 @@ class RegisterViewTestCase(TestCase):
         self.client = APIClient()
 
     def test_register_user(self):
-        # Dados de exemplo para o usuário
         user_data = {
             "name": "testuser",
             "email": "t@t.com",
@@ -17,5 +16,17 @@ class RegisterViewTestCase(TestCase):
         }
 
         response = self.client.post('/users/register/', user_data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+
+class LoginViewTestCase(APITestCase):
+    def test_login_failure(self):
+        invalid_login_data = {
+            "email": "rururururu",
+            "password": "rararara"
+        }
+
+        response = self.client.post('/users/login/', invalid_login_data, format='json')
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
