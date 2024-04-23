4f8308bb721bdfecfb2cea474986b530eac78645
ryanrare <ryan.hollmes@gmail.com>
2024-04-23 11:52:46 -0300

    (test) creating test regiter

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,22 @@
+import json
 from django.test import TestCase
+from rest_framework.test import APIClient
+from rest_framework import status
+from .models import User
 
-# Create your tests here.
+
+class RegisterViewTestCase(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_register_user(self):
+        # Dados de exemplo para o usuário
+        user_data = {
+            "name": "testuser",
+            "email": "t@t.com",
+            "password": "t"
+        }
+
+        response = self.client.post('/api/register/', user_data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
