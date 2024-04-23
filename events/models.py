2923c594288dc760dc02a2eff256d28fbafcc6aa
ryanrare <ryan.hollmes@gmail.com>
2024-04-23 01:56:50 -0300

    (feat) creating user_events model

--- events/models.py
+++ events/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from users.models import User
 
 
 class Event(models.Model):
@@ -12,3 +13,14 @@ class Event(models.Model):
     class Meta:
         verbose_name = 'event'
         db_table = 'event'
+
+
+class UserEvent(models.Model):
+    user = models.ForeignKey(User, verbose_name="User", on_delete=models.CASCADE)
+    event = models.ForeignKey(Event, verbose_name="Event", on_delete=models.CASCADE)
+    registration_date = models.DateTimeField("Registration Date", auto_now_add=True)
+
+    class Meta:
+        verbose_name = "user event"
+        verbose_name_plural = "user events"
+        db_table = "user_event"
